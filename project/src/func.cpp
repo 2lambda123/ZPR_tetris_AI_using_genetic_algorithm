2bbb6f26570b4eea78ac3abe5260595545bbe022
Damian Kolaska <d.kolaska@stud.elka.pw.edu.pl>
2020-11-19 01:57:42 +0100

    File structure, simple unit test using Catch2

---
+++ project/src/func.cpp
@@ -0,0 +1,5 @@
+#include "func.hpp"
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ project/src/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "func.hpp"
+
+int main() {
+    std::cout << "Hello! Did you know that 2 + 2 is " << add(2, 2) << std::endl;
+    return 0;
+}
