1dc036d14916a6223e5720dcd0b1efde586ce952
Damian Kolaska <damian.kolaska99@gmail.com>
2020-11-30 02:03:15 +0100

    Changes to CMake configuration

---
+++ tests/test_gui_tetris_integration.cpp
@@ -0,0 +1,24 @@
+#include "gui.hpp"
+#include "tetris.hpp"
+
+int main() {
+    bool closed = false;
+    sf::Clock clock;
+    GUI gui(800, 900);
+    Tetris tetris;
+
+    clock.restart();
+    while (!closed) {
+        sf::Event event;
+        while (gui.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) closed = true;
+        }
+        if (clock.getElapsedTime() > sf::seconds(1.0f)) {
+            tetris.tick();
+            clock.restart();
+        }
+        gui.update(tetris.getGrid());
+        gui.draw();
+    }
+    return 0;
+}
